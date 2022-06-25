e996b2789f18600ddfdb69b7c2d72c8bfd60daf1
OmegaWei1984 <omegawei1984@gmail.com>
2022-06-25 14:26:55 +0800

    add more content to programming_in_lua/ch04_string.lua

--- programming_in_lua/ch04_string.lua
+++ programming_in_lua/ch04_string.lua
@@ -46,18 +46,50 @@ print(xml2)
 
 -- 4.3
 local function insert(str, position, add)
-    local pre = ""
-    local suf = ""
-    local index = 0;
-    if position < 0 and position >= -#str then
-        position = position + #str + 1
-    elseif position > #str or position == 0 then
-        return nil;
+    if position < 0 then
+        position = position % -#str
+        position = position +  1 + #str
+    elseif position > #str then
+        position = position % #str
     end
-    -- if position
-    
+    -- print(#str, position)
+    -- print("pre", string.sub(str, 0, position - 1))
+    -- print("suf", string.sub(str, position, #str))
+    local pre = string.sub(str, 0, position - 1)
+    local suf = string.sub(str, position, #str)
     return pre .. add .. suf
 end
 print(insert("hello world", 1, "start: "))
-print(insert("hello wolrd", 7, "small"))
+print(insert("hello wolrd", 7, "small "))
 print(insert("hello world", -1, "!"))
+print(insert("hello world", #"hello world", "!"))
+print(insert("hello world", -21, "|"))
+
+-- 4.5
+local function remove(str, position, num)
+    if position < 0 then
+        position = position % -#str
+        position = position +  1 + #str
+    elseif position > #str then
+        position = position % #str
+    end
+    local pre = str:sub(0, position - 1)
+    local tmp = math.min(#str, position + num)
+    local suf = str:sub(tmp, #str)
+    return pre .. suf;
+end
+print(remove("hello world", 7, 4))
+
+-- 4.7
+local function isPalindrome(str)
+    if (#str % 2) == 1 then
+        return str:sub(0, (#str // 2)) == string.reverse(str:sub((#str // 2) + 2, #str))
+    else
+        return str:sub(0, #str / 2) == string.reverse(str:sub((#str / 2) + 1, #str))
+    end
+end
+print(isPalindrome("abba"))
+print(isPalindrome("abcba"))
+print(isPalindrome("aaa"))
+print(isPalindrome("a"))
+print(isPalindrome("abc"))
